0e6653ed00c15d7c0bbd0a0f74f76238a6f9bcf7
nihce <nihce@github.com>
2019-11-15 18:26:16 +0100

    Create transaction_generator.js

---
+++ src/transaction_generator.js
@@ -0,0 +1,36 @@
+//IMPORTS
+const rp = require('request-promise');
+const sha256 = require('sha256');
+const uuid = require('uuid/v1');
+const random = require('random');
+
+const minTimeout = 500;
+const maxTimeout = 5000;
+
+generator();
+
+function generator() {
+    const newTransaction = createRandomTransaction();
+    const singlePromise = {
+        uri: 'http://localhost:5001/broadcastTransaction',
+        method: 'POST',
+        body: newTransaction,
+        json: true
+    };
+    rp(singlePromise)
+    .then(function (body) {
+        console.log(body);
+        console.log();
+        setTimeout(generator, random.int(minTimeout, maxTimeout));
+
+    }).catch((err) => {console.log(err)});
+};
+
+function createRandomTransaction(){
+    var transaction = new Object();
+    transaction.amount = random.int(0, 100);
+    transaction.sender  = sha256(uuid().split('-').join(''));
+    transaction.recipient = sha256(uuid().split('-').join(''));
+    console.log(JSON.stringify(transaction));
+    return transaction;
+}
